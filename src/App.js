c5b54e47eeee7821a0c43d9903de7600a580e76b
jogindrakumar <joginder9868@outlook.com>
2020-01-26 15:58:51 +0530

    AddClarfaiApi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './containers/Navigation/Navigation';
 import Logo from './containers/Logo/Logo';
 import Rank from './containers/Rank/Rank';
 import ImageLineForm from './containers/ImageLineForm/ImageLineForm';
+import FormRecognition from './containers/FormRecognition/FormRecognition';
 
 import './App.css';
-
+const app = new Clarifai.App({
+  apiKey: '8b6bcf95aa2c40818be5bca1f6d56b04'
+ });
 const ParticlesOptions = {
   
     particles: {
@@ -21,6 +25,32 @@ const ParticlesOptions = {
   
 }
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state ={
+      input: '',
+      imageUrl: ''
+    }
+  }
+  onInputChange = (event) =>{
+    this.setState({input:event.target.value});
+  }
+  onButtonSubmit = () =>{
+    this.setState({imageUrl:this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input).then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
+  
   render(){
     return (
       <div className="App">
@@ -31,9 +61,11 @@ class App extends Component {
         <Navigation />
          <Logo />
          <Rank />
-        <ImageLineForm />
+        <ImageLineForm 
+        onInputChange={this.onInputChange}
+        onButtonSubmit={this.onButtonSubmit} />
         
-       {/* <FormRecognition /> */}
+        <FormRecognition imageUrl={this.state.imageUrl} /> 
         
       </div>
     );
--- src/containers/ImageLineForm/ImageLineForm.js
+++ src/containers/ImageLineForm/ImageLineForm.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './ImageLineForm.css';
-const ImageLineForm = () =>{
+const ImageLineForm = ({onInputChange ,onButtonSubmit}) =>{
     return (
             <div>
                 <p className='f3'>
                 {'magic of brain that find your face in your pictures'}</p>
                 <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text'/>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
                    
-    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>                     
+    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>                     
                    </div>
                 </div>
             </div>
